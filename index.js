4aeedc22d470cb2c6f0eea6f564784ebdf1f2ac8
KamyllaAlmeida <kamylla.florencio@gmail.com>
2018-11-20 22:30:01 +0000

    Step 1: Introduce Task Object.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Arrays to keep track of each task's state
+
+
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
-// Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
 }
+
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
@@ -23,9 +29,12 @@ function logTaskState(taskIndex) {
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+// DRIVER CODE BELOW
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+
+console.log(tasks);
+
